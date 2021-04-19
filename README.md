23e2cb5fff27aa910f64c8c07fe485888d7b1d95
CK13 Soohwan Kim <soowh91@gmail.com>
2021-04-19 11:44:05 +0900

    fix error

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Cert Manager 임시 설치 가이드
 
 ## 폐쇄망 설치 가이드
-* 외부 네트워크 통신이 가능한 환경에서 0.preset-cn.sh를 이용하여 이미지 및 패키지 다운로드 후 1.2.install-cert-cn.sh를 이용하여 폐쇄망에 CERT MANAGER 환경 구성
+* 외부 네트워크 통신이 가능한 환경에서 0.preset-cn.sh를 이용하여 이미지 및 패키지 다운로드 후 1.2.install-cert-cn.sh를 이용하여 폐쇄망에 Cert Manager 환경 구성
 * 외부 네트워크 환경 스크립트 실행순서
     ```bash
     $ cd manifest
--- manifest/1.2.install-cert-cn.sh
+++ manifest/1.2.install-cert-cn.sh
@@ -16,4 +16,9 @@ sudo docker load < img/cert-manager-cainjector:$CERT_MANAGER_VERSION.tar
 sudo docker tag quay.io/jetstack/cert-manager-cainjector:$CERT_MANAGER_VERSION $REGISTRY/jetstack/cert-manager-cainjector:$CERT_MANAGER_VERSION
 sudo docker push $REGISTRY/jetstack/cert-manager-cainjector:$CERT_MANAGER_VERSION
 
+## Change image registry
+sed -i 's/quay.io\/jetstack\/cert-manager-controller:'${CERT_MANAGER_VERSION}'/'${REGISTRY}'\/jetstack\/cert-manager-controller:'${CERT_MANAGER_VERSION}'/g' cert-manager-${CERT_MANAGER_VERSION}.yaml
+sed -i 's/quay.io\/jetstack\/cert-manager-webhook:'${CERT_MANAGER_VERSION}'/'${REGISTRY}'\/jetstack\/cert-manager-webhook:'${CERT_MANAGER_VERSION}'/g' cert-manager-${CERT_MANAGER_VERSION}.yaml
+sed -i 's/quay.io\/jetstack\/cert-manager-cainjector:'${CERT_MANAGER_VERSION}'/'${REGISTRY}'\/jetstack\/cert-manager-cainjector:'${CERT_MANAGER_VERSION}'/g' cert-manager-${CERT_MANAGER_VERSION}.yaml
+
 kubectl apply -f cert-manager-${CERT_MANAGER_VERSION}.yaml
