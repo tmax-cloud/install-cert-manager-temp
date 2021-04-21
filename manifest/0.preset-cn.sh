82c22a03b6827c40747dbfb90202a0d36888eebc
CK13 Soohwan Kim <soowh91@gmail.com>
2021-04-21 10:45:30 +0900

    modify variable expression

--- manifest/0.preset-cn.sh
+++ manifest/0.preset-cn.sh
@@ -4,18 +4,18 @@ if [ ! -d img ]; then
 fi
 
 ## Pull image: kube-rbac-proxy
-sudo docker pull gcr.io/kubebuilder/kube-rbac-proxy:$KUBE_RBAC_PROXY_VERSION 
-sudo docker save gcr.io/kubebuilder/kube-rbac-proxy:$KUBE_RBAC_PROXY_VERSION > img/kubebuilder_kube-rbac-proxy_$KUBE_RBAC_PROXY_VERSION.tar 
+sudo docker pull gcr.io/kubebuilder/kube-rbac-proxy:${KUBE_RBAC_PROXY_VERSION} 
+sudo docker save gcr.io/kubebuilder/kube-rbac-proxy:${KUBE_RBAC_PROXY_VERSION} > img/kubebuilder_kube-rbac-proxy_${KUBE_RBAC_PROXY_VERSION}.tar 
 
 ## Pull images: 0. cert-manager.yaml
-sudo docker pull quay.io/jetstack/cert-manager-controller:$CERT_MANAGER_VERSION
-sudo docker save quay.io/jetstack/cert-manager-controller:$CERT_MANAGER_VERSION > img/cert-manager-controller:$CERT_MANAGER_VERSION.tar
+sudo docker pull quay.io/jetstack/cert-manager-controller:${CERT_MANAGER_VERSION}
+sudo docker save quay.io/jetstack/cert-manager-controller:${CERT_MANAGER_VERSION} > img/cert-manager-controller:${CERT_MANAGER_VERSION}.tar
 
-sudo docker pull quay.io/jetstack/cert-manager-webhook:$CERT_MANAGER_VERSION
-sudo docker save quay.io/jetstack/cert-manager-webhook:$CERT_MANAGER_VERSION > img/cert-manager-webhook:$CERT_MANAGER_VERSION.tar
+sudo docker pull quay.io/jetstack/cert-manager-webhook:${CERT_MANAGER_VERSION}
+sudo docker save quay.io/jetstack/cert-manager-webhook:${CERT_MANAGER_VERSION} > img/cert-manager-webhook:${CERT_MANAGER_VERSION}.tar
 
-sudo docker pull quay.io/jetstack/cert-manager-cainjector:$CERT_MANAGER_VERSION
-sudo docker save quay.io/jetstack/cert-manager-cainjector:$CERT_MANAGER_VERSION > img/cert-manager-cainjector:$CERT_MANAGER_VERSION.tar
+sudo docker pull quay.io/jetstack/cert-manager-cainjector:${CERT_MANAGER_VERSION}
+sudo docker save quay.io/jetstack/cert-manager-cainjector:${CERT_MANAGER_VERSION} > img/cert-manager-cainjector:${CERT_MANAGER_VERSION}.tar
 
 ## Download binary files and yaml files
 curl -L https://github.com/jetstack/cert-manager/releases/download/${CERT_MANAGER_VERSION}/cert-manager.yaml > cert-manager-${CERT_MANAGER_VERSION}.yaml
