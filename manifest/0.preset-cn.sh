45d623cb28228da231b5e81bd30076eb8eb042fa
CK13 Soohwan Kim <soowh91@gmail.com>
2021-05-18 18:48:14 +0900

    fix error

--- manifest/0.preset-cn.sh
+++ manifest/0.preset-cn.sh
@@ -5,13 +5,13 @@ fi
 
 ## Pull images: 0. cert-manager.yaml
 sudo docker pull quay.io/jetstack/cert-manager-controller:${CERT_MANAGER_VERSION}
-sudo docker save quay.io/jetstack/cert-manager-controller:${CERT_MANAGER_VERSION} > img/cert-manager-controller:${CERT_MANAGER_VERSION}.tar
+sudo docker save quay.io/jetstack/cert-manager-controller:${CERT_MANAGER_VERSION} > img/cert-manager-controller_${CERT_MANAGER_VERSION}.tar
 
 sudo docker pull quay.io/jetstack/cert-manager-webhook:${CERT_MANAGER_VERSION}
-sudo docker save quay.io/jetstack/cert-manager-webhook:${CERT_MANAGER_VERSION} > img/cert-manager-webhook:${CERT_MANAGER_VERSION}.tar
+sudo docker save quay.io/jetstack/cert-manager-webhook:${CERT_MANAGER_VERSION} > img/cert-manager-webhook_${CERT_MANAGER_VERSION}.tar
 
 sudo docker pull quay.io/jetstack/cert-manager-cainjector:${CERT_MANAGER_VERSION}
-sudo docker save quay.io/jetstack/cert-manager-cainjector:${CERT_MANAGER_VERSION} > img/cert-manager-cainjector:${CERT_MANAGER_VERSION}.tar
+sudo docker save quay.io/jetstack/cert-manager-cainjector:${CERT_MANAGER_VERSION} > img/cert-manager-cainjector_${CERT_MANAGER_VERSION}.tar
 
 ## Download binary files and yaml files
 curl -L https://github.com/jetstack/cert-manager/releases/download/${CERT_MANAGER_VERSION}/cert-manager.yaml > cert-manager-${CERT_MANAGER_VERSION}.yaml
